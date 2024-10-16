34e9ad4922fd652a4d5eb814a294494637896140
marandra <marcelo.raschi@gmail.com>
2024-10-16 12:30:36 +0200

    Remove ppa of ansible

--- scripts/linux/install_dependencies.sh
+++ scripts/linux/install_dependencies.sh
@@ -8,8 +8,6 @@ cwd="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
 apt="sudo apt -y"
 $apt update
 
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-
 install_from_package_list() {
   export DEBIAN_FRONTEND=noninteractive
   packages="$(awk '! /^ *(#|$)/' "$1")"
